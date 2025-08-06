c39a949833c9c46b73bdf57c76d94a1ee456b123
Bramdon Santiago <bramdsantiago@gmail.com>
2025-08-06 17:22:44 -0600

    Creando Header, Footer, Home y creando y agregando el resto de las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,21 @@ export const routes: Routes = [
         path: 'home',
         loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
     },
+    {
+        path: 'catalogo',
+        loadComponent: () => import('./pages/store/store.component').then(m => m.StoreComponent)
+    },
+    {
+        path: 'promociones',
+        loadComponent: () => import('./pages/promotions/promotions.component').then(m => m.PromotionsComponent)
+    },
+    {
+        path: 'blog',
+        loadComponent: () => import('./pages/blog/blog.component').then(m => m.BlogComponent)
+    },
+    {
+        path: 'contacto',
+        loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent)
+    },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,33 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{html,ts}"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}"
+    "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          sm: '600px',
+          md: '992px',
+          lg: '1160px',
+          xl: '1400px',
+        },
+      },
+    },
   },
   plugins: [],
 }
+
