dd3b8c297e152069f565f1be175d46f651c8e75b
Bramdon Santiago <bramdsantiago@gmail.com>
2025-08-07 13:53:45 -0600

    Agregando seccion testimonios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import Swiper from 'swiper';
+import { Pagination, Autoplay } from 'swiper/modules';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +9,34 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  swiperTestimonials!: Swiper;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.initializeSwiper();
+    }
+  }
+
+  initializeSwiper() {
+    this.swiperTestimonials = new Swiper('.swiper-testimonials', {
+      modules: [Pagination, Autoplay],
+      autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+      },
+    });
+  }
 }
