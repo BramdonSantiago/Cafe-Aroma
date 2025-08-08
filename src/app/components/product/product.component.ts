cb6e6f18041cb4554b382d4580eb0c87143a98e9
Bramdon Santiago <bramdsantiago@gmail.com>
2025-08-07 18:11:47 -0600

    Agregando blog detalle

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { CartService } from '../../services/cart/cart.service';
 
 @Component({
   selector: 'app-product',
@@ -8,4 +9,10 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductComponent {
   @Input() product!: any;
+
+  constructor(private cartService: CartService) {}
+
+  showCart() {
+    this.cartService.showCart();
+  }
 }
