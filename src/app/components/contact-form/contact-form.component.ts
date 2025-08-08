a6c459246ad06abe4fde5a1ed4e95cf5146b1576
Bramdon Santiago <bramdsantiago@gmail.com>
2025-08-08 11:23:39 -0600

    Ajustes hero home mobile

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormGroup, Validators, ValidationErrors, FormControl, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
+  contactForm = new FormGroup({
+    nameInput: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    mailInput: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    messageInput: new FormControl('', [Validators.required, Validators.minLength(10)]),
+  });
 
+  getInvalidControls() {
+    return Object.values(this.contactForm.controls).forEach(control => {
+      control.markAllAsTouched();
+    })
+  }
+
+  onSubmit() {
+    if (!this.contactForm.valid) {
+      this.getInvalidControls();
+    } else {
+      console.log("Enviar");
+    }
+  }
 }
